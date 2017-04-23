c6581106425ff484a777e35a10f10a688a7317aa
Biggie Emmanuel <bigolu96@gmail.com>
2017-04-22 23:55:59 -0400

    fat commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    
+    $('#joined').click(function(event){
+      $('#join').html(' \
+        <div class="valign-wrapper modal-content"> \
+          <div class="row center"> \
+            <i class="large material-icons">done</i> \
+            <p>Success!</p> \
+          </div> \
+        </div> \
+        <div class="modal-footer"> \
+          <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Go to Dashboard</a> \
+        </div> \
+      ');
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
